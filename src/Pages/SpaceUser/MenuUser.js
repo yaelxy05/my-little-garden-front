8ec7ca808f6d06f21dd08c082d97ddd3d871aacb
yaelx <yaelx@localhost>
2022-03-09 11:05:25 +0100

    add icon menu user

--- src/Pages/SpaceUser/MenuUser.js
+++ src/Pages/SpaceUser/MenuUser.js
@@ -1,30 +1,44 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
-
+// Import fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faSeedling,
+  faRectangleList,
+  faCarrot
+} from "@fortawesome/free-solid-svg-icons";
 // Import picture
-import Avatar from '../../assets/img/avatar/portfolio-img.jpg'
+import Avatar from "../../assets/img/avatar/portfolio-img.jpg";
 
 function MenuUser() {
   return (
-    <> 
+    <>
       <aside className="menu_user">
-          <div className="menu_user-img">
-              <img src={Avatar} alt="avatar profil" />
-              <p>Yael</p>
-          </div>
-     
-      <ul>
-        <li>
-          <Link to="/espace-utilisateur/mon-profil" >
-            Mon profil
-          </Link>
-        </li>
-        <li>Ajouter un semis</li>
-        <li>Liste de mes semis</li>
-        <li>Mon potager</li>
-      </ul>
-    </aside>
-    <Outlet />
+        <div className="menu_user-img">
+          <img src={Avatar} alt="avatar profil" />
+          <p>HUE Yael</p>
+        </div>
+
+        <ul>
+          <li>
+            <FontAwesomeIcon icon={faUser} />
+            <Link to="/espace-utilisateur/mon-profil">Mon profil</Link>
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faSeedling} />
+            Ajouter un semis
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faRectangleList} />
+            Liste de mes semis
+          </li>
+          <li>
+          <FontAwesomeIcon icon={faCarrot} />
+            Mon potager</li>
+        </ul>
+      </aside>
+      <Outlet />
     </>
   );
 }
