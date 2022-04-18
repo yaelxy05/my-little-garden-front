38afae14f8e66944851f175f4e1e0d3f809a9793
yaelx <yaelx@localhost>
2022-04-18 22:34:44 +0200

    add map list potager of bdd

--- src/Pages/SpaceUser/MyGarden/Potager/index.js
+++ src/Pages/SpaceUser/MyGarden/Potager/index.js
@@ -1,45 +1,78 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 // import SCSS
 import "./potager.scss";
+// Import package
+import axios from "axios";
 
 function Potager() {
+  const [listPotager, setListPotager] = useState([]);
+
+  const token = localStorage.getItem("token");
+  const API_URLS = process.env.REACT_APP_API_URL;
+  useEffect(() => {
+    const getPotagerList = () => {
+      axios
+        .get(`${API_URLS}/potager`, {
+          headers: {
+            Authorization: "Bearer " + token,
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+        })
+        .then((response) => {
+          console.log(response);
+          setListPotager(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getPotagerList();
+  }, []);
   return (
     <section className="potager">
       <h2>Plan du potager</h2>
-          <div className="potager_plan">
-              <div className="potager_carre--box">
-                  <h3>potager 1</h3>
-              <div className="potager_carre"></div>
-              </div>
-              <div className="potager_carre--box">
-                  <h3>potager 1</h3>
-              <div className="potager_carre"></div>
-              </div>
-              <div className="potager_carre--box">
-                  <h3>potager 1</h3>
-              <div className="potager_carre"></div>
-              </div>
-              <div className="potager_carre--box">
-                  <h3>potager 1</h3>
-              <div className="potager_carre"></div>
-              </div>
-              <div className="potager_carre--box">
-                  <h3>potager 1</h3>
-              <div className="potager_carre"></div>
-              </div>
-              <div className="potager_carre--box">
-                  <h3>potager 1</h3>
-              <div className="potager_carre"></div>
-              </div>
-              <div className="potager_carre--box">
-                  <h3>potager 1</h3>
-              <div className="potager_carre"></div>
-              </div>
-              <div className="potager_carre--box">
-                  <h3>potager 1</h3>
-              <div className="potager_carre"></div>
-              </div>
+      <div className="potager_plan">
+        {listPotager &&
+          listPotager.map((list, index) => {
+            if (list.size === 33) {
+              return (
+                <div key={index} className="potager_carre--box">
+                  <h3>{list.name}</h3>
+                  <div className="potager_carre carre33"></div>
+                </div>
+              );
+            } else if (list.size === 25) {
+              return (
+                <div key={index} className="potager_carre--box">
+                  <h3>{list.name}</h3>
+                  <div className="potager_carre carre25"></div>
+                </div>
+              );
+            } else if (list.size === 24) {
+              return (
+                <div key={index} className="potager_carre--box">
+                  <h3>{list.name}</h3>
+                  <div className="potager_carre carre25"></div>
+                </div>
+              );
+            } else if (list.size === 35) {
+              return (
+                <div key={index} className="potager_carre--box">
+                  <h3>{list.name}</h3>
+                  <div className="potager_carre carre25"></div>
+                </div>
+              );
+            }
+            else {
+                return (
+                  <div key={index} className="potager_carre--box">
+                    <h3>{list.name}</h3>
+                    <div className="potager_carre"></div>
+                  </div>
+                );
+              }
+          })}
       </div>
     </section>
   );
